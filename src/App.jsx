288e05a3da3e37bb0b53ef67518da005fe1cadc8
cqrlosArch <diaz.flores.c@gmail.com>
2020-06-26 20:59:12 +0200

    26/06/2020

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+
+import './styles/styles.scss'
+
+const App = () => {
+    return (
+        <div>
+          <Header/>
+        </div>
+    )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+let path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+require("dotenv").config();
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const devMode = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: "./dist",
+    port:3000
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        loaders: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: true
+            },
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true },
+          },
+        ],
+      },
+    ],
+  },
+  mode: devMode ? "development" : "production",
+  resolve: {
+    extensions: [".jsx", ".mjs", ".js", ".json", ".css", ".scss"],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: devMode ? "[name].css" : "[name].[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css",
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
